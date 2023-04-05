6bb8c87c0e673c7e61b84eed38d54371f4dbbb26
josh <github@junk.josh.com>
2023-04-04 23:24:20 -0400

    Airtable logging in programming script working.

--- programming/program.py
+++ programming/program.py
@@ -6,8 +6,12 @@ import shutil
 
 import hashlib
 
-firmware_file_name = "tsl-calibre-msp.txt"
+# the following are for airtable stuff
+import keyring
+import http.client
+import json
 
+firmware_file_name = "tsl-calibre-msp.txt"
 
 # // The format that we use to store timestamps
 #
@@ -42,81 +46,139 @@ with open(firmware_file_name, 'rb') as file_to_check:
 firmware_hash = hashlib.md5(data).hexdigest()
 
 print( f"Firmware hash is {firmware_hash}\n")
-        
-# Create the image to burn into the unit by appending a timestamp to the firmware
 
-with open('output.txt','wb') as wfd:
-    # prepend a time stamp (in TI HEX format)
+# repeat unill user quits or error
 
-    # get the current time
-    t = time.localtime()
-    wfd.write("@1800\n".encode())
-    wfd.write(f"{t.tm_sec:02d} {t.tm_min:02d} {t.tm_hour:02d} {t.tm_wday:02d} {t.tm_mday:02d} {t.tm_mon:02d} {t.tm_year%100:02d}\n".encode())
-        
-    # ...and add firmware file into the image file
-    # note that the firmware comes last becuase the TI tools add a "q" to the end of this file. 
-    with open('tsl-calibre-msp.txt','rb') as rfd:
-        shutil.copyfileobj(rfd, wfd)
+while True: 
 
-# If you ever need to read the commisioned time out of a unit, you can use the command...
-# MSP430Flasher.exe -j fast -r [commisioned_time.txt,0x1800-0x1806] -z [VCC]
-# Do note that the timestamp does not a a century field, so you will have to infer what century the unit was commisioned
-# in using other factors like how dusty it is. 
+    serialno = input ("Serial Number (or blank to exit)?")
 
+    # Check if the string is empty
+    if not serialno:
+        print("Goodbye.")
+        exit(0)
+            
+    # Create the image to burn into the unit by appending a timestamp to the firmware
 
-# Here is the meat where we...
-# 1. Grab the device UUID and ID from the MSP430 chip. This will let us keep a record of the serial number and what version of the chip this unit has
-# 2. Burn the current time/date into the info memory as described above
-# 3. Burn the firmware
+    with open('output.txt','wb') as wfd:
+        # prepend a time stamp (in TI HEX format)
 
-# -j fast means use the fastest clock speed so programming will go as quickly as possible (it still takes a couple seconds)
-# -e ERASE_MAIN prepares the FRAM to recieve the firmware download
-# -v verifies the contents of the FRAM match the firmware file
-# -z [VCC] leaves the device powered up via the VCC pin (You should see the "First Start" message on the LCD display)
+        # get the current time
+        t = time.localtime()
+        wfd.write("@1800\n".encode())
+        wfd.write(f"{t.tm_sec:02d} {t.tm_min:02d} {t.tm_hour:02d} {t.tm_wday:02d} {t.tm_mday:02d} {t.tm_mon:02d} {t.tm_year%100:02d}\n".encode())
+            
+        # ...and add firmware file into the image file
+        # note that the firmware comes last becuase the TI tools add a "q" to the end of this file. 
+        with open('tsl-calibre-msp.txt','rb') as rfd:
+            shutil.copyfileobj(rfd, wfd)
 
-command = f"MSP430Flasher.exe -j fast -r [uuid.txt,0x1A04-0x1A0a] -r [device.txt,0x1a04-0x1a07] -e ERASE_MAIN -w output.txt -v -z [VCC]"
-
-print("STARING COMMAND:")
-print(command)
-
-result = subprocess.run(command, capture_output=False)
-
-# Check the return code and print the output
-if result.returncode != 0:
-    print("MSPFlasher failed!")
-    exit()
+    # If you ever need to read the commisioned time out of a unit, you can use the command...
+    # MSP430Flasher.exe -j fast -r [commisioned_time.txt,0x1800-0x1806] -z [VCC]
+    # Do note that the timestamp does not a a century field, so you will have to infer what century the unit was commisioned
+    # in using other factors like how dusty it is. 
 
 
-# Lets grab the UUID and device info and make into a serial number
+    # Here is the meat where we...
+    # 1. Grab the device UUID and ID from the MSP430 chip. This will let us keep a record of the serial number and what version of the chip this unit has
+    # 2. Burn the current time/date into the info memory as described above
+    # 3. Burn the firmware
 
-with open("uuid.txt","r") as f:
-    # throw away the address line
-    f.readline()
-    # grab the UUID without trailing newline
-    uuid_raw = f.readline().rstrip()
+    # -j fast means use the fastest clock speed so programming will go as quickly as possible (it still takes a couple seconds)
+    # -e ERASE_MAIN prepares the FRAM to recieve the firmware download
+    # -v verifies the contents of the FRAM match the firmware file
+    # -z [VCC] leaves the device powered up via the VCC pin (You should see the "First Start" message on the LCD display)
 
-with open("device.txt","r") as f:
-    # throw away the address line
-    f.readline()
-    # grab the UUID without trailing newline
-    device_raw = f.readline().rstrip()
+    command = f"MSP430Flasher.exe -j fast -r [uuid.txt,0x1A04-0x1A0a] -r [device.txt,0x1a04-0x1a07] -e ERASE_MAIN -w output.txt -v -z [VCC]"
 
-# concat the two and and remove embeded spaces
-serial_no = ( device_raw + uuid_raw ).replace(" ","")
+    print("STARING COMMAND:")
+    print(command)
 
-print( f"Serial number is {serial_no}\n")
+    result = subprocess.run(command, capture_output=False)
 
-input("Press Enter to continue...")
+    # Check the return code and print the output
+    if result.returncode != 0:
+        print("MSPFlasher failed!")
+        exit(1)
 
-# s.replace(" ", "")
 
-# read device UID
-# MSP430Flasher.exe -j FAST -r [dump.txt,0x1A0A-0x1A11]
+    # Lets grab the UUID and device info and make into a serial number
 
-# MSP430Flasher.exe -j fast -e ERASE_MAIN -r [uuid.txt,0x1A04-0x1A0a] -r [device.txt,0x1a04-0x1a07] -w tsl-calibre-msp.txt -v -z [VCC]
-# MSP430Flasher.exe -j fast -r [info.txt,0x1800-0x1900] -z [RESET,VCC]
+    with open("uuid.txt","r") as f:
+        # throw away the address line
+        f.readline()
+        # grab the UUID without trailing newline
+        uuid_raw = f.readline().rstrip()
 
-# MSP430FR4133 F0h 81h
-# MSP430FR4132 F1h 81h
-# MSP430FR4131 F2h 81h
+    with open("device.txt","r") as f:
+        # throw away the address line
+        f.readline()
+        # grab the device info without trailing newline
+        device_raw = f.readline().rstrip()
+
+    # concat the two and and remove embeded spaces
+    deviceid = ( device_raw + uuid_raw ).replace(" ","")
+
+    print( f"Device ID is {deviceid}\n")
+
+    print( "Adding record to airtable...\n")
+
+    # Airtable API host
+    host = "api.airtable.com"
+    # This is the base and table ID of the TSL units table
+    endpoint = "/v0/app11MZ4rXXpEyFnj/tblunHqmlHFKtvaZ1"
+
+    # Data for the new record to be created
+    data = {
+        "fields": {
+            "Firmware": firmware_hash,
+            "DeviceID": deviceid,
+            "Orders": []
+        }
+    }
+
+    # OMG python is so ugly. 
+
+    data_raw = '''
+        {{
+          "records": [
+            {{
+              "fields": {{
+                "Serialnumber": "{}",
+                "Firmware": "{}",
+                "DeviceID": "{}"
+              }}
+            }}
+          ]
+        }}
+    '''
+
+    data = data_raw.format(serialno,firmware_hash,deviceid)
+
+    # Convert data to JSON format
+    json_data = json.dumps(data)
+
+    # Reads the Airtable API access token that was stored into KeyRing durring setup.
+    airtable_token = keyring.get_password("tsl-airtable", "token")
+
+    # Headers for the API request. 
+    headers = {
+        "Authorization": f"Bearer {airtable_token}",
+        "Content-Type": "application/json"
+    }
+
+    # Make the API request to create a new record
+    conn = http.client.HTTPSConnection(host)
+    conn.request("POST", endpoint, data, headers)
+    response = conn.getresponse()
+
+    # Check if the request was successful
+    if response.status == 200:
+        print("New record created successfully!")
+    else:
+        print("Error creating new record")
+        print(response.read().decode())
+        exit(1)
+
+    
 
