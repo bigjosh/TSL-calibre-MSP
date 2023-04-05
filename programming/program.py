35ba7fc8337511a1fd90a070d3e6597ae463c22d
josh <github@junk.josh.com>
2023-04-05 16:53:49 -0400

    Clean up program.py

--- programming/program.py
+++ programming/program.py
@@ -1,3 +1,7 @@
+
+# use this script to program new units
+# sorry it is so ugly, but i wanted it to be pythonic
+
 import subprocess
 
 import time
@@ -11,43 +15,47 @@ import keyring
 import http.client
 import json
 
+# Airtable API host
+host = "api.airtable.com"
+# This is the base and table ID of the TSL units table
+endpoint = "/v0/app11MZ4rXXpEyFnj/tblunHqmlHFKtvaZ1"
+# Reads the Airtable API access token that was stored into KeyRing durring setup
+airtable_token = keyring.get_password("tsl-airtable", "token")
+# Headers for the API request. 
+airtable_headers = {
+    "Authorization": f"Bearer {airtable_token}",
+    "Content-Type": "application/json"
+}
+
+# OMG python is so ugly. 
+airtable_record_template = '''
+    {{
+      "records": [
+        {{
+          "fields": {{
+            "Serialnumber": "{}",
+            "Firmware": "{}",
+            "DeviceID": "{}"
+          }}
+        }}
+      ]
+    }}
+'''
+
+# firmware in TI hex format
 firmware_file_name = "tsl-calibre-msp.txt"
-
-# // The format that we use to store timestamps
-#
-#struct __attribute__((__packed__)) rv3032_time_block_t {
-#    byte sec_bcd;
-#    byte min_bcd;
-#    byte hour_bcd;
-#    byte weekday_bcd;
-#    byte date_bcd;
-#    byte month_bcd;
-#    byte year_bcd;
-#};
-
-# Create a file snippet in TI hex format that has the current time/date
-
-def create_timestamp_file():
-    # get the current time
-    t = time.localtime()
-
-    # write the timecode snippet to a file called "timecode.txt"
-    with open("timestamp.txt", "w") as f:
-        f.write("@1800\n")
-        f.write(f"{t.tm_sec:02d} {t.tm_min:02d} {t.tm_hour:02d} {t.tm_wday:02d} {t.tm_mday:02d} {t.tm_mon:02d} {t.tm_year%100:02d}\n")
 
 # load the firmware file into memory variable `data`
 
-with open(firmware_file_name, 'rb') as file_to_check:
-    # read contents of the file
-    data = file_to_check.read()
+with open(firmware_file_name, 'rb') as file:
+    data = file.read()
 
 # cacluate the hash of the firmware file
 firmware_hash = hashlib.md5(data).hexdigest()
 
 print( f"Firmware hash is {firmware_hash}\n")
 
-# repeat unill user quits or error
+# repeat programming cycle until user quits or error
 
 while True: 
 
@@ -58,31 +66,31 @@ while True:
         print("Goodbye.")
         exit(0)
             
-    # Create the image to burn into the unit by appending a timestamp to the firmware
+    # Create the combined image to burn into the unit that includes both timestamp and firmware
 
     with open('output.txt','wb') as wfd:
-        # prepend a time stamp (in TI HEX format)
-
         # get the current time
         t = time.localtime()
+        
+        # prepend a time stamp (in TI HEX format)
+        # 1800 is the begining of "information memory" FRAM. 
         wfd.write("@1800\n".encode())
         wfd.write(f"{t.tm_sec:02d} {t.tm_min:02d} {t.tm_hour:02d} {t.tm_wday:02d} {t.tm_mday:02d} {t.tm_mon:02d} {t.tm_year%100:02d}\n".encode())
             
-        # ...and add firmware file into the image file
-        # note that the firmware comes last becuase the TI tools add a "q" to the end of this file. 
+        # ...and append firmware file into the image file
+        # note that the firmware comes last becuase the TI tools add a "q" to the end of this file.
         with open('tsl-calibre-msp.txt','rb') as rfd:
             shutil.copyfileobj(rfd, wfd)
 
     # If you ever need to read the commisioned time out of a unit, you can use the command...
     # MSP430Flasher.exe -j fast -r [commisioned_time.txt,0x1800-0x1806] -z [VCC]
-    # Do note that the timestamp does not a a century field, so you will have to infer what century the unit was commisioned
+    # Do note that the timestamp does not have a century field, so you will have to infer what century the unit was commisioned
     # in using other factors like how dusty it is. 
 
-
     # Here is the meat where we...
-    # 1. Grab the device UUID and ID from the MSP430 chip. This will let us keep a record of the serial number and what version of the chip this unit has
-    # 2. Burn the current time/date into the info memory as described above
-    # 3. Burn the firmware
+    # 1. Grab the device UUID and ID from the MSP430 chip. This will let us keep a record of the serial number and what version of the chip this unit has.
+    # 2. Erase the FRAM. 
+    # 3. Burn the combined image into the unit.
 
     # -j fast means use the fastest clock speed so programming will go as quickly as possible (it still takes a couple seconds)
     # -e ERASE_MAIN prepares the FRAM to recieve the firmware download
@@ -103,6 +111,7 @@ while True:
 
 
     # Lets grab the UUID and device info and make into a serial number
+    # these are in TI HEX format so we have to throw away the first line and then strip out all whitespace from the second line to get a clean hex digit string
 
     with open("uuid.txt","r") as f:
         # throw away the address line
@@ -123,53 +132,11 @@ while True:
 
     print( "Adding record to airtable...\n")
 
-    # Airtable API host
-    host = "api.airtable.com"
-    # This is the base and table ID of the TSL units table
-    endpoint = "/v0/app11MZ4rXXpEyFnj/tblunHqmlHFKtvaZ1"
-
-    # Data for the new record to be created
-    data = {
-        "fields": {
-            "Firmware": firmware_hash,
-            "DeviceID": deviceid,
-            "Orders": []
-        }
-    }
-
-    # OMG python is so ugly. 
-
-    data_raw = '''
-        {{
-          "records": [
-            {{
-              "fields": {{
-                "Serialnumber": "{}",
-                "Firmware": "{}",
-                "DeviceID": "{}"
-              }}
-            }}
-          ]
-        }}
-    '''
-
-    data = data_raw.format(serialno,firmware_hash,deviceid)
-
-    # Convert data to JSON format
-    json_data = json.dumps(data)
-
-    # Reads the Airtable API access token that was stored into KeyRing durring setup.
-    airtable_token = keyring.get_password("tsl-airtable", "token")
-
-    # Headers for the API request. 
-    headers = {
-        "Authorization": f"Bearer {airtable_token}",
-        "Content-Type": "application/json"
-    }
+    airtable_record_request = airtable_record_template.format(serialno,firmware_hash,deviceid)
 
     # Make the API request to create a new record
     conn = http.client.HTTPSConnection(host)
-    conn.request("POST", endpoint, data, headers)
+    conn.request("POST", endpoint, airtable_record_request, airtable_headers)
     response = conn.getresponse()
 
     # Check if the request was successful
